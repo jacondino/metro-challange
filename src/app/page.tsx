7d4274906669db201b0c8e01b973988901c19816
Anderson Jacondino <anderson.jacondino15@gmail.com>
2025-02-13 17:25:01 -0300

    feat: AJUSTA VARIAVEL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { LinkQuestion } from "@/components/LinkQuestion";
 
 export default function Home() {
+  const testee = process.env.NEXT_PUBLIC_TESTE || "NÃO DEU BOM";
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-[50px] m-4">Choose a Question:</h1>
+      <h1 className="text-[50px] m-4">Choose a Question: {testee}</h1>
       <LinkQuestion number={1} />
       <LinkQuestion number={2} />
       <LinkQuestion number={3} />
